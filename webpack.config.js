b80e44c7d0c472c7af667c1aa7e4c942f30bfc37
谢峻 <jy.xj@alibaba-inc.com>
2019-06-27 15:30:25 +0800

    初始化仓库

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+/**
+ * Created by junxie on 18/5/27.
+ */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry: {
+        pageOne: './src/todoList/index.jsx',
+        pageTwo: './src/trafficLight/index.jsx',
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: '[name].js',
+        hotUpdateChunkFilename: 'hot/hot-update.js',
+        hotUpdateMainFilename: 'hot/hot-update.json'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: 'index.html'
+        })
+    ],
+    devtool: 'source-map',
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react', '@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(less|css)$/,
+                use: ['style-loader', 'css-loader', 'less-loader'],
+            },
+            {
+                test: /\.js$/,
+                loader: 'eslint-loader',
+                enforce: 'pre',
+                include: [path.resolve(__dirname, 'src')],
+                options: {
+                    formatter: require('eslint-friendly-formatter')
+                }
+            }
+        ]
+    },
+    resolve: {
+        // you can now require('file') instead of require('file.coffee')
+        extensions: ['.js', '.json', '.jsx']
+    }
+};
